310fb12c85e71bbab8713a9a9a6edb763e37bc1e
wbbhacker <2404016117@qq.com>
2020-11-21 22:58:20 +0800

    添加数据源

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/blog/**/*.md',
+        typeName: 'BlogPost',
+      },
+    },
+    
+  ]
 }
