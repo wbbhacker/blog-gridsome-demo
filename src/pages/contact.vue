3065b2fe9348c1c4271804f69d0b572123e55b94
wangbinbinc@enn.cn <2404016117@qq.com>
2020-12-09 20:58:36 +0800

    联系页面开发

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -29,7 +29,6 @@
           <!-- Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work. -->
           <!-- WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address! -->
           <!-- To use the contact form, your site must be on a live web host with PHP! The form will not work locally! -->
-          <form name="sentMessage" id="contactForm" novalidate>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls">
                 <label>Name</label>
@@ -39,6 +38,7 @@
                   placeholder="Name"
                   id="name"
                   required
+                  v-model="form.name"
                   data-validation-required-message="Please enter your name."
                 />
                 <p class="help-block text-danger"></p>
@@ -53,6 +53,7 @@
                   placeholder="Email Address"
                   id="email"
                   required
+                  v-model="form.email"
                   data-validation-required-message="Please enter your email address."
                 />
                 <p class="help-block text-danger"></p>
@@ -69,6 +70,7 @@
                   placeholder="Phone Number"
                   id="phone"
                   required
+                  v-model="form.phone"
                   data-validation-required-message="Please enter your phone number."
                 />
                 <p class="help-block text-danger"></p>
@@ -83,6 +85,7 @@
                   placeholder="Message"
                   id="message"
                   required
+                  v-model="form.message"
                   data-validation-required-message="Please enter a message."
                 ></textarea>
                 <p class="help-block text-danger"></p>
@@ -91,13 +94,12 @@
             <br />
             <div id="success"></div>
             <button
-              type="submit"
               class="btn btn-primary"
               id="sendMessageButton"
+              @click="onSubmitClick"
             >
               Send
             </button>
-          </form>
         </div>
       </div>
     </div>
@@ -107,8 +109,34 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'ContactPage'
+  name: 'ContactPage',
+  data(){
+    return {
+      form:{
+        name:'',
+        email: '',
+        phone: '',
+        message: ''
+      }
+    }
+  },
+  methods: {
+    async onSubmitClick(){
+      try{
+        const {data} = await axios({
+          method:'POST',
+          url:'http://localhost:1337/contacts',
+          data:this.form
+        })
+        console.log('发送成功')
+      }catch{
+        console.log('发送失败')
+      }
+    }
+  },
 }
 </script>
 
